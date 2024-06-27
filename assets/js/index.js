18fdcb3f736a205efdeb29c5c71e98584d8f905a
Siria <sirifranco00@gmail.com>
2024-06-27 16:50:00 -0700

    feat: general visual

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict'
+
+    // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Bucle sobre ellos y evitar el envío
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+// Limpiar el formulario
+function clearForm() {
+    document.getElementById("productForm").reset();
+    document.getElementById("productForm").classList.remove('was-validated');
+}
